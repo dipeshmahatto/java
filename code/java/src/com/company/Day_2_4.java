3c751193f49236012e62f3ea7d47dbfce376bc63
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-02 23:31:28 +0545

    Day_2

---
+++ code/java/src/com/company/Day_2_4.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Day_2_4 {
+    public static void main(String[] args) {
+        int age = 35;
+        byte date = 23;
+        char name ='D';
+        float f1 = 0.25f;
+        double d1 = 20.54d;
+        long ages = 544464646446l;
+        short agge = 15;
+        boolean a = true;
+        System.out.println(date);
+        System.out.println(name);
+        System.out.println(ages);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(agge);
+        System.out.println(age);
+        String str = "Dipesh";
+        System.out.println(str);
+    }
+}
