b9a7b192a204e836e0cb8eafa9858078f8b024f3
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-02 23:48:49 +0545

    Day_2

--- code/java/src/com/company/Day_2_5.java
+++ code/java/src/com/company/Day_2_5.java
@@ -1,7 +1,27 @@
 package com.company;
+import java.util.Scanner;
 
 public class Day_2_5 {
     public static void main(String[] args){
-        int a = 5;
+        System.out.println("Taking input from user:");
+        Scanner sc = new Scanner(System.in);
+
+//  System.out.println("Enter number 1");
+// int a = sc.nextInt();
+// float a = sc.nextFloat();
+// System.out.println("Enter number 2");
+// int b = sc.nextInt();
+// float b = sc.nextFloat();
+// int sum = a +b;
+// float sum = a +b;
+// System.out.println("The sum of these numbers is");
+// System.out.println(sum);
+// boolean b1 = sc.hasNextInt();
+// System.out.println(b1);
+// String str = sc.next();
+
+        String str = sc.nextLine();
+        System.out.println(str);
+
     }
 }
