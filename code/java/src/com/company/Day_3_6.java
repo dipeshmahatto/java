49cbd11d73e0078828e9cad757caaeed77444b5c
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-03 20:31:44 +0545

    Day-3

---
+++ code/java/src/com/company/Day_3_6.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class Day_3_6 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter your 5 subjects marks to get your percentage:");
+        System.out.println("Enter your first subject mark");
+        float first = s.nextFloat() ;
+
+
+    }
+}
