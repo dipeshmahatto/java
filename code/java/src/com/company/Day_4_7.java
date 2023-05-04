d9f3abae98268fff3a0723eb3481494fe8179031
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-04 15:07:17 +0545

    Day-4

--- code/java/src/com/company/Day_4_7.java
+++ code/java/src/com/company/Day_4_7.java
@@ -1,18 +1,25 @@
 package com.company;
-import java.io.StringReader;
 import java.util.Scanner;
 public class Day_4_7 {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter your name:");
+//        System.out.println("Enter your name:");
+//
+//        String name = s.next();
+//
+//        System.out.print("Hello ");
+//        System.out.print(name);
+//        System.out.print(", have a good day.");
 
-        String name = s.next();
+        System.out.println("Enter the number in kilometer");
+        double kilo = s.nextDouble();
 
-        System.out.print("Hello ");
-        System.out.print(name);
-        System.out.print(", have a good day.");
+        double mile = kilo * 0.62137119;
+
+        System.out.print("Given value is converted into miles :");
+        System.out.println(mile);
 
     }
 }
