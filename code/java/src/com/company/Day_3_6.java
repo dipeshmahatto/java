4da6ae3e44b8dbdeb5c77e43cbcc25a20531f02f
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-04 14:16:59 +0545

    Day-4

--- code/java/src/com/company/Day_3_6.java
+++ code/java/src/com/company/Day_3_6.java
@@ -6,9 +6,25 @@ public class Day_3_6 {
         Scanner s = new Scanner(System.in);
 
         System.out.println("Enter your 5 subjects marks to get your percentage:");
-        System.out.println("Enter your first subject mark");
+        System.out.println("Enter your first subject mark:");
         float first = s.nextFloat() ;
 
+        System.out.println("Enter your second subject mark:");
+        float second = s.nextFloat();
+
+        System.out.println("Enter your Third subject mark:");
+        float Third = s.nextFloat();
+
+        System.out.println("Enter your fourth subject mark:");
+        float fourth = s.nextFloat();
+
+        System.out.println("Enter your fivth subject mark:");
+        float fivth = s.nextFloat();
+
+        float percentage = ((first+second+Third+fourth+fivth)/500)*100;
+
+        System.out.print(percentage);
+        System.out.println("%");
 
     }
 }
