3514ee3cbead0120b846c2dae86dbffc1a414048
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-05 21:39:43 +0545

    Day-5

---
+++ code/java/src/com/company/Day_5.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class Day_5 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter integer number you want to check odd or even:");
+        int check = s.nextInt();
+
+        if (check%2==0){
+            System.out.println("Even");
+        } else {
+            System.out.println("odd");
+        }
+
+
+
+    }
+}
