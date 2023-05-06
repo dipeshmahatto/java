0f8265bb9bb236674321aef79c5f07fdd37065eb
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-06 10:52:17 +0545

    Day-6

--- code/java/src/com/company/Day_6_12.java
+++ code/java/src/com/company/Day_6_12.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.Scanner;
 
 public class Day_6_12 {
     public static void main(String[] args) {
@@ -7,12 +8,25 @@ public class Day_6_12 {
         //                System.out.println(a);
 
         //        Question-2
-                        char grade = 'A' ;
-                        grade = (char)(grade + 8);
-        System.out.println(grade);
+//                        char grade = 'A' ;
+//                        grade = (char)(grade + 8);
+//        System.out.println(grade);
+//
+//        grade = (char)(grade-8);
+//        System.out.println(grade);
 
-        grade = (char)(grade-8);
-        System.out.println(grade);
+//        Question-3
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your first number:");
+        int a = s.nextByte();
+        System.out.println("Enter your second number:");
+        int b = s.nextInt();
+
+        if (a>b){
+            System.out.println(a + " is the greater number than "+ b);
+        } else {
+            System.out.println(b + " is the greater number than " + a);
+        }
 
 
     }
