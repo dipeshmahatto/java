a4403046c8ab61a2758606c6430a50f7173a1ccb
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-10 23:14:43 +0545

    Day-7

---
+++ code/java/src/com/company/Day_7_15.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class Day_7_15 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+//        String name = "Dipesh";
+        String name = s.nextLine();
+        System.out.println(name);
+
+    }
+}
