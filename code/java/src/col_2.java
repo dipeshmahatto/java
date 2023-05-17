ae4e4e2f4597a84826e1a6ea8f2d650c18af89bd
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-17 12:14:05 +0545

    col-2

---
+++ code/java/src/col_2.java
@@ -0,0 +1,36 @@
+public class col_2 {
+    public static void main(String[] args) {
+//        int sum = 0;
+//        int i;
+//        for (i=1;i<=10;i++){
+//            sum = sum + i;
+//        }
+//        int j=1;
+//        for(;j<=10;){
+//            sum = sum + j;
+//            j++;
+//        }
+
+//        int k;
+//        for (k = 1; k <= 10; k++) {
+//            if (k % 2 == 1) {
+//                continue;
+//            }
+//            sum = sum + k;
+//    }
+
+//        int z = 1;
+//        do {
+//            System.out.println(z);
+//            z++;
+//        } while (z <= 10);
+        //        System.out.println("Even :" + sum);
+
+        for(int i= 1;i<=3;i++){
+           for (int j=1;j<=i;j++){
+               System.out.print("*");
+           }
+            System.out.println();
+        }
+    }
+}
