05c0e43cb7cf984ef642e3eec4890e486250732f
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-25 23:25:09 +0545

    Day_14 recursion

--- code/java/src/com/company/Day_13_33_5.java
+++ code/java/src/com/company/Day_13_33_5.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Day_13_33_5 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 }
