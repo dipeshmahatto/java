61475c0ebc8e7489e9ccc7ef5d868f2c7c3f53fd
Dipesh mahato <103894820+dipeshmahatto@users.noreply.github.com>
2023-05-25 07:06:38 +0545

    Day_13 Pratice set

--- code/java/src/com/company/Day_13_33_4.java
+++ code/java/src/com/company/Day_13_33_4.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Day_13_33_4 {
+    static void pattern(int a) {
+        for (int i = a; i > 0; i--) {
+            for (int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of rows :");
+        int n = s.nextInt();
+        System.out.println("PATTERN :");
+        pattern(n);
+    }
 }
