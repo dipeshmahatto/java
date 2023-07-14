28d0735da08e31df897a2a9280e7844dd9f28acc
dipesh <dipeshmahatto@gmail.com>
2023-07-14 18:55:13 +0545

    graph

--- code/java/src/Adder.java
+++ code/java/src/Adder.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Adder implements ActionListener {
     JTextField tf1, tf2, tf3;
